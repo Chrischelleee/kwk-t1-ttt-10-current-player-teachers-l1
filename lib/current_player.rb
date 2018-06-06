a24d1b09b5aede74cb51d9adf0d8c1bd29cb7607
Chrischelle Acelajado <chrischelleacelajado@gmail.com>
2018-06-06 04:44:37 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,24 @@
+#turn count method
+# * determine whether whose turn it is already
+def turn_count(board)
+      counter = 0
+
+  board.each do |board|
+
+    if board != " "
+      counter += 1
+      # puts "there are #{counter} space/s occupied"
+    end
+  end
+  counter
+end
+
+#current player
+def current_player(board)
+
+  if turn_count.Odd
+    puts "X"
+  # else
+  #   return "X"
+  end
+end
