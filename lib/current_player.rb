52c5007c20f67df0c6eb6b065f18dbb1c20d1c00
Chrischelle Acelajado <chrischelleacelajado@gmail.com>
2018-06-06 04:57:26 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,23 @@
+#turn count method
+# * determine whether whose turn it is already
+def turn_count(board)
+      counter = 0
+
+  board.each do |board|
+
+    if board != " "
+      counter += 1
+    end
+  end
+  counter
+end
+
+#current player
+def current_player(board)
+
+  if turn_count(board) % 2 == 0
+    return "X"
+else
+  return "O"
+  end
+end
